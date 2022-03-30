1261d12e5ec13117aeae3e10d11371ed31751bc5
Arash Nabbi <nabbi.arash@gmail.com>
2022-03-29 20:44:57 -0400

    Add Fig2D

--- main/Figure2.R
+++ main/Figure2.R
@@ -5,7 +5,7 @@
 library(survminer)
 library(survival)
 
-#2A
+#Fig2A
 mainpath <- "~/git/iMATRIX-Atezo_Biomarker/"
 datapath <- paste0(mainpath,"data/")
 
@@ -17,18 +17,20 @@ setwd(mainpath)
 
 source(paste0(mainpath, "R/ggplot2_theme.R"))
 source(paste0(mainpath, "R/Heatmap_functions.R"))
+source(paste0(mainpath, "R/Splot_function.R"))
 
 metadata <- read.csv(file.path(datapath,"metadata.csv"), header = T, stringsAsFactors = F, check.names = F)
 
+message("summary for CD8 staining (IHC) in the iMATRIX-Atezo:")
 summary(metadata$IHC_CD8)
+message("summary for CD3 staining (IHC) in the iMATRIX-Atezo:")
 summary(metadata$IHC_CD3)
-
 
 metadata$CD8group <- NA
 metadata$CD8group[metadata$IHC_CD8 >= 1.242] <- "High"
 metadata$CD8group[metadata$IHC_CD8 > 0.070 &
                   metadata$IHC_CD8 < 1.242] <- "Intermediate"
-IND_metadata$CD8group[IND_metadata$IHC_CD8 <= 0.070] <- "Low"
+metadata$CD8group[metadata$IHC_CD8 <= 0.070] <- "Low"
 
 sfit <- survfit(Surv(TRTDUR, progressed)~ CD8group, data= metadata)
 
@@ -49,7 +51,7 @@ pdf(file = paste0(plotpath,"Fig2A.pdf"),
 kmplot_CD8
 dev.off()
 
-# 2B
+# Fig2B
 metadata$CD3group <- NA
 metadata$CD3group[metadata$IHC_CD3 >= 3.05] <- "High"
 metadata$CD3group[metadata$IHC_CD3 > 0.16 &
@@ -74,10 +76,10 @@ pdf(file = paste0(plotpath,"Fig2B.pdf"),
 kmplot_CD3
 dev.off()
 
-# 2C
-
+# Fig2C
 metadata_CD8 <- read.csv(file.path(datapath,"metadata_CD8CIBERSORT.csv"), header = T, stringsAsFactors = F, check.names = F)
 
+message("summary for CD8 estimate from cibersort in the iMATRIX-Atezo:")
 summary(metadata_CD8$T_cell_CD8_CIBERSORT_ABS)
 
 metadata_CD8$CD8group <- NA
@@ -104,5 +106,35 @@ pdf(file = paste0(plotpath,"Fig2C.pdf"),
 kmplot_ciber
 dev.off()
 
-# 2D
+# Fig2D
+th_data <- read.csv(file.path(datapath,"treehouse_CD8.csv"), header = T, stringsAsFactors = F, 
+                    check.names = F, row.names = NULL)
 
+
+th_data$CD8Level <- NA
+th_data$CD8Level[th_data$CD8_CIBERSORT >= 0.10383] <- "High"
+th_data$CD8Level[th_data$CD8_CIBERSORT < 0.10383 & 
+                 th_data$CD8_CIBERSORT > 0] <- "Intermediate"
+th_data$CD8Level[th_data$CD8_CIBERSORT == 0] <- "Low"
+
+#recode 0 for visualization
+th_data$CD8_CIBERSORT[ th_data$CD8_CIBERSORT == 0 ] <- 0.00001
+
+th_data$cohort <- th_data$diseasetype
+th_data$group <- "TH"
+
+#Splot
+th_mediandf <- median.cohorts.fx(th_data)
+list_sorted_df <- sort.cohorts.fx(th_data, th_mediandf)
+cd8_Splot <- Splot.fx(list_sorted_df, "Data from Treehouse (n = 1851)") + 
+  annotation_logticks(sides = "l") + scale_y_continuous(trans = "log10",
+                                                        breaks = c(0.00001, 0.0001, 0.001, 0.01, 0.1),
+                                                        label = c(0, 0.0001, 0.001, 0.01, 0.1)) 
+
+pdf(file = paste0(plotpath,"Fig2D.pdf"),
+    width = 25, 
+    height = 12,
+    useDingbats = FALSE,
+    onefile = FALSE)
+cd8_Splot + theme(legend.position = "right")
+dev.off()
